a7617f1c5f193d48d2e14e463ea66e8e1356a361
LuxuriousGerg <georggalileo21@mail.ru>
2024-05-28 21:04:21 +0300

    Теперь 2 класса и починил отрицательные числа

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) {
         BillCalculator calculator = new BillCalculator();
